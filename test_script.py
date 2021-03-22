38a2788eb90b01f5747438a612f9e68ad08ebe1b
tazeek <tazeek.rakib@gmail.com>
2021-03-22 23:06:32 +0800

    Datetime object replaced

--- test_script.py
+++ test_script.py
@@ -8,18 +8,18 @@ import MetaTrader5 as mt5
 if not mt5.initialize():
     print("initialize() failed")
     mt5.shutdown()
+
+intialize_today = datetime.now()
+intialize_today.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
  
-# request connection status and parameters
-print(mt5.terminal_info())
-# get data on MetaTrader 5 version
-print(mt5.version())
- 
-# request 1000 ticks from EURAUD
-eurgbp_ticks = mt5.copy_ticks_from("EURGBP", datetime(2020,1,28,13), 1000, mt5.COPY_TICKS_ALL) 
+# request 1000 ticks from EURGBP
+eurgbp_ticks = mt5.copy_ticks_from("EURGBP", intialize_today, 1000, mt5.COPY_TICKS_ALL) 
 
 # shut down connection to MetaTrader 5
 mt5.shutdown()
  
+print('OUT HERE!')
+
 #DATA
 print('eurgbp_ticks(', len(eurgbp_ticks), ')')
 for val in eurgbp_ticks[:10]: print(val)
