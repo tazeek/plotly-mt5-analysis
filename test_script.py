46feded1129c32c6f3205426818f4b8b3788c47d
tazeek <tazeek.rakib@gmail.com>
2021-03-23 23:01:42 +0800

    Using new mt5 function

--- test_script.py
+++ test_script.py
@@ -9,11 +9,10 @@ if not mt5.initialize():
     print("initialize() failed")
     mt5.shutdown()
 
-intialize_today = datetime.now()
-intialize_today.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
- 
-# request 1000 ticks from EURGBP
-eurgbp_ticks = mt5.copy_ticks_from("EURGBP", intialize_today, 1000, mt5.COPY_TICKS_ALL) 
+now_time = datetime.now()
+start_day = datetime.now().replace(hour = 0, minute = 0, second = 0)
+
+eurgbp_ticks = mt5.copy_ticks_range("EURGBP", start_day, now_time)
 
 # shut down connection to MetaTrader 5
 mt5.shutdown()
@@ -22,5 +21,5 @@ print('OUT HERE!')
 
 #DATA
 print('eurgbp_ticks(', len(eurgbp_ticks), ')')
-for val in eurgbp_ticks[:10]: print(val)
+for val in eurgbp_ticks[-1]: print(val)
  
