2fd7c773118abfcc796907d0bbe38c5bc63f6652
tazeek <tazeek.rakib@gmail.com>
2021-04-26 20:22:50 +0800

    Function added successfully

--- copy_rates_test.py
+++ copy_rates_test.py
@@ -1,36 +1,36 @@
 from datetime import datetime
 import MetaTrader5 as mt5
 import pandas as pd
-import pytz
  
-# import the 'pandas' module for displaying data obtained in the tabular form
-# import pytz module for working with time zone
+def find_minutes_passed(current):
+    start_day = datetime(current.year, current.month, current.day)
+    time_delta = (current - start_day)
+    
+    return int(time_delta.total_seconds()/60)
+
  
 # establish connection to MetaTrader 5 terminal
 if not mt5.initialize():
     print("initialize() failed, error code =",mt5.last_error())
     quit()
- 
-# set time zone to UTC
-timezone = pytz.timezone("Etc/UTC")
-today = datetime.today()
 
-# create 'datetime' object in UTC time zone to avoid the implementation of a local time zone offset
-utc_from = datetime(today.year, today.month, today.day, tzinfo=timezone)
 
-# get 10 EURUSD H4 bars starting from 01.10.2020 in UTC time zone
+current = datetime.today()
+bars_to_get = find_minutes_passed(current)
+
+# Get all the bars since the start of the day
 currency = "EURUSD"
-rates = mt5.copy_rates_from(currency, mt5.TIMEFRAME_D1, utc_from, 12)
- 
-# shut down connection to the MetaTrader 5 terminal
-mt5.shutdown()
- 
+rates = mt5.copy_rates_from(currency, mt5.TIMEFRAME_M1, current, bars_to_get)
+
 # create DataFrame out of the obtained data
-rates_frame = pd.DataFrame(rates)
 # convert time in seconds into the datetime format
+rates_frame = pd.DataFrame(rates)
 rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
-                           
+
 # display data
 print(currency)
 print("\nDisplay dataframe with data")
 print(rates_frame)  
+ 
+# shut down connection to the MetaTrader 5 terminal
+mt5.shutdown()
