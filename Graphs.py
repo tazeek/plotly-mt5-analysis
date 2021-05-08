b67493cff549ece026d2793a02140b76345265cb
tazeek <tazeek.rakib@gmail.com>
2021-05-08 12:00:37 +0800

    Red and green lines added for percentage change graph

--- Graphs.py
+++ Graphs.py
@@ -165,6 +165,30 @@ class Graphs:
         )
 
         percentage_change_fig.add_hline(
+            y=3,
+            line_dash="dash",
+            line_color="red"
+        )
+
+        percentage_change_fig.add_hline(
+            y=-3,
+            line_dash="dash",
+            line_color="red"
+        )
+
+        percentage_change_fig.add_hline(
+            y=10,
+            line_dash="dash",
+            line_color="green"
+        )
+
+        percentage_change_fig.add_hline(
+            y=-10,
+            line_dash="dash",
+            line_color="green"
+        )
+
+        percentage_change_fig.add_hline(
             y=0,
             line_dash="solid",
             line_color="black"
