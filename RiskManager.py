3ad2a4520ad4748a8b703cad0f569f323cc88967
tazeek <tazeek.rakib@gmail.com>
2021-05-12 22:50:52 +0800

    Risk manager completed

--- RiskManager.py
+++ RiskManager.py
@@ -2,17 +2,18 @@ import math
 
 class RiskManager:
 
-    def calculate_remaining_trades(para_dict):
+    def calculate_remaining_trades(self, para_dict):
 
-        amount = para_dict['balance'] - para_dict['buffer']
-        risk_vol = para_dict['pip_loss'] - para_dict['leverage']
+        amount = para_dict['current_balance'] - para_dict['buffer_balance']
+        risk_vol = para_dict['pip_loss'] * para_dict['leverage']
 
-        return Math.ceil(amount / risk_vol)
+        return math.ceil(amount / risk_vol)
 
 
-    def calculate_pips_profit(para_dict):
+    def calculate_pips_profit(self, para_dict):
 
         profit_aim = para_dict['profit']
-        min_trades = para_dict['min_trades']
+        min_trades = para_dict['min_trades_profit']
+        leverage = para_dict['leverage']
 
-        ...
+        return math.ceil(profit_aim / (min_trades * leverage))
