14247493cf29630ecb72eb6cfc048d64c9040f23
tazeek <tazeek.rakib@gmail.com>
2021-05-14 01:52:30 +0800

    New callbacks added in controller

--- controller.py
+++ controller.py
@@ -15,7 +15,7 @@ def register_callbacks(app):
     def update_forex_analyzer(value):
         forex_analyzer.update_forex_pair(value)
         graph_generator.update_currency(value)
-        
+
         return [value]
 
     @app.callback(
@@ -28,4 +28,29 @@ def register_callbacks(app):
 
         return [
             graph_generator.plot_candlesticks_weekly(last_30days_stats)
+        ]
+    
+    @app.callback(
+        [Output("tick-volatility-fig","figure")],
+        [Input("current-currency", "data")]
+    )
+    def display_tick_volatility(value):
+        
+        day_stats = forex_analyzer.get_daily_stats()
+        start_day = forex_analyzer.get_start_day()
+
+        return [
+            graph_generator..plot_tick_volume_fullday(day_stats, start_day)
+        ]
+    
+    @app.callback(
+        [Output("pip-size-histogram-fig","figure")],
+        [Input("current-currency", "data")]
+    )
+    def display_tick_volatility(value):
+        
+        day_stats = forex_analyzer.get_daily_stats()
+
+        return [
+            graph_generator.plot_pip_difference_graph(day_stats)
         ]
