9e8b3a185092e5d63dba8d7c7470c350d5c81747
tazeek <tazeek.rakib@gmail.com>
2021-05-14 00:15:25 +0800

    Dropdown code added

--- layout.py
+++ layout.py
@@ -1,8 +1,25 @@
 import dash_core_components as dcc
 import dash_html_components as html
 
+def _generate_dropdown():
+
+    forex_list = ['GBPUSD','EURGBP','EURUSD','AUDUSD','AUDJPY','USDJPY']
+
+    dropdown_options = [{'label': currency, 'value': currency} for currency in forex_list]
+    print(dropdown_options)
+
+    return html.Div([
+        dcc.Dropdown(
+            id='currency-dropdown',
+            options=dropdown_options,
+            value='GBPUSD'
+        )
+    ])
+
 def generate_layout():
 
+    _generate_dropdown()
+    
     return html.Div([
         html.H1(children='Hello World')
     ])
