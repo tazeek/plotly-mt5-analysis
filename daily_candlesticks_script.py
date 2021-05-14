988ddfd3d4ff3d51698087c1e756d9fbfd0d5960
tazeek <tazeek.rakib@gmail.com>
2021-05-14 21:26:47 +0800

    Forex pairs fetched

--- daily_candlesticks_script.py
+++ daily_candlesticks_script.py
@@ -1,1 +1,10 @@
 from ForexAnalyzer import ForexAnalyzer
+
+# Fetch the required Forex pairs
+forex_pairs = []
+
+with open('forex_pairs.txt') as f:
+    for line in f.readlines():
+        forex_pairs.append(line.strip())
+
+print(forex_pairs)
