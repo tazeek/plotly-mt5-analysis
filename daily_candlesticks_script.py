54eb2ce8489a383b93c9ddaa908d9afb9935e10f
tazeek <tazeek.rakib@gmail.com>
2021-05-14 21:30:15 +0800

    Width of candlestick fetched successfully

--- daily_candlesticks_script.py
+++ daily_candlesticks_script.py
@@ -7,4 +7,16 @@ with open('forex_pairs.txt') as f:
     for line in f.readlines():
         forex_pairs.append(line.strip())
 
-print(forex_pairs)
+forex_analyzer = ForexAnalyzer()
+
+# Get the stats of each forex pair and store in dictionary
+candlesticks_width_dict = {}
+
+for pair in forex_pairs:
+    
+    forex_analyzer.update_forex_pair(pair)
+
+    pair_stats = forex_analyzer.get_d1_stats()
+    candlesticks_width_dict[pair] = pair_stats['width_candlestick']
+
+print(candlesticks_width_dict)
