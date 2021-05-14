551a4888280d97cac2908c7f5f30fecf64b9ee2c
tazeek <tazeek.rakib@gmail.com>
2021-05-14 15:22:10 +0800

    Graph successfullyl created

--- Graphs.py
+++ Graphs.py
@@ -36,6 +36,32 @@ class Graphs:
         
         return None
 
+    def plot_candlestick_today(self, data):
+
+        candlestick_today_fig = go.Figure(
+            data=[
+                go.Candlestick(
+                    x=[data['time']],
+                    open=[data['open']], 
+                    high=[data['high']],
+                    low=[data['low']], 
+                    close=[data['close']]
+                )
+            ]
+        )
+
+        candlestick_today_fig.update_layout(
+            title=f"{self._currency} - Today",
+            xaxis_title="Date",
+            yaxis_title="Price",
+            hovermode='x',
+            yaxis_tickformat='.5f',
+            xaxis_rangeslider_visible=False,
+            showlegend=False,
+        )
+
+        return candlestick_today_fig
+
     def plot_candlesticks_weekly(self, data):
 
         def _create_info(data_row):
