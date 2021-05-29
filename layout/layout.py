6eb08e590820727494e18f81f58b7ce76dc40c12
tazeek <tazeek.rakib@gmail.com>
2021-05-29 17:43:48 +0800

    Layout updated one last time in terms of candlestick chart placement

--- layout/layout.py
+++ layout/layout.py
@@ -147,8 +147,8 @@ def generate_layout():
         _loading_figure_layout('percentage-changes-fig'),
         _loading_figure_layout('agg-percentage-fig'),
         html.Hr(),
-        _loading_figure_layout('candlestick-1H-fig', draw_config),
         _loading_figure_layout('candlestick-15M-fig', draw_config),
+        _loading_figure_layout('candlestick-1H-fig', draw_config),
         html.Hr(),
         _loading_figure_layout('rsi-fig'),
         _loading_figure_layout('bull-bear-fig')
