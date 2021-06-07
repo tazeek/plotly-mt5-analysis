cc697c3e9ff8f91a7e708172d98bd0df4d643c35
tazeek <tazeek.rakib@gmail.com>
2021-06-07 17:24:29 +0800

    Order of candlesticks display updated

--- layout/layout.py
+++ layout/layout.py
@@ -145,11 +145,11 @@ def generate_layout():
         _loading_figure_layout('heatmap-price-changes-fig'),
         _loading_figure_layout('percentage-changes-fig'),
         html.Hr(),
-        _loading_figure_layout('candlestick-15M-fig', draw_config),
         _loading_figure_layout('rsi-fig'),
         _loading_figure_layout('bull-bear-fig'),
         html.Hr(),
+        _loading_figure_layout('candlestick-4H-fig', draw_config),
         _loading_figure_layout('candlestick-1H-fig', draw_config),
-        _loading_figure_layout('candlestick-4H-fig', draw_config)
+        _loading_figure_layout('candlestick-15M-fig', draw_config),
         
     ])
