ac9618253f9159c38b1016c3a6d34c1c4fb3843f
tazeek <tazeek.rakib@gmail.com>
2021-08-31 00:07:09 +0800

    Event table properly extracted

--- economics_events_scraper.py
+++ economics_events_scraper.py
@@ -1,19 +1,25 @@
 from bs4 import BeautifulSoup
 
-import requests
+import urllib.request
 
 class ForexFactoryScraper:
 
     def __init__(self, month_select):
-        self._url = 'https://www.forexfactory.com/calendar?month=' + month_select
+        self._url = 'https://www.forexfactory.com/calendar.php?month=' + month_select
 
-    def extract_html_data(self):
-        response = requests.get(self._url)
-        data = response.text
-        soup = BeautifulSoup(data, 'lxml')
+    def _extract_html_data(self):
+
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        response = opener.open(self._url)
+        result = response.read().decode('utf-8', errors='replace')
+
+        return BeautifulSoup(result,"html.parser")
 
     def begin_extraction(self):
-        ...
+        parsed_html = self._extract_html_data()
+        table = parsed_html.find_all("tr", class_="calendar_row")
+        return None
 
 ff_scraper = ForexFactoryScraper('this')
-ff_scraper.extract_html_data()
+ff_scraper.begin_extraction()
