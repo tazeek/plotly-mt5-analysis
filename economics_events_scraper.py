ff366d577e01056ec593cf2297ec66a8e66b2bdb
tazeek <tazeek.rakib@gmail.com>
2021-08-30 23:54:17 +0800

    URL extracted successfully

--- economics_events_scraper.py
+++ economics_events_scraper.py
@@ -1,5 +1,19 @@
+from bs4 import BeautifulSoup
+
+import requests
+
 class ForexFactoryScraper:
 
-    ...
+    def __init__(self, month_select):
+        self._url = 'https://www.forexfactory.com/calendar?month=' + month_select
 
-ff_scraper = ForexFactoryScraper()
+    def extract_html_data(self):
+        response = requests.get(self._url)
+        data = response.text
+        soup = BeautifulSoup(data, 'lxml')
+
+    def begin_extraction(self):
+        ...
+
+ff_scraper = ForexFactoryScraper('this')
+ff_scraper.extract_html_data()
